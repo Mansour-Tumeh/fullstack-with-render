6427d3b62a812e9a2eeac29f5eb9cda4df1f83ec
Mansour-Tumeh <46924057+Mansour-Tumeh@users.noreply.github.com>
2022-12-05 23:56:23 +0100

    Update alluser.js

--- client/src/components/alluser.js
+++ client/src/components/alluser.js
@@ -8,7 +8,7 @@ function Getallusers(props){
     })
     useEffect(()=>{
         try{
-            axios.get('http://localhost:4000/getallusers').then(res=>{
+            axios.get('/getallusers').then(res=>{
                 setuser({
                     students:res.data
                 });
